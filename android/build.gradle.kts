a284173a209ceb71913dfca457f1de01d073b687
WorkerOfArea51 <area.51.ancientworkers@gmail.com>
2026-06-12 13:18:36 +0600

    build: inject fallback namespace for subprojects missing namespace

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -19,6 +19,21 @@ subprojects {
     project.evaluationDependsOn(":app")
 }
 
+subprojects {
+    afterEvaluate {
+        if (plugins.hasPlugin("com.android.application") || plugins.hasPlugin("com.android.library")) {
+            val android = extensions.findByName("android")
+            if (android != null) {
+                val baseExtension = android as? com.android.build.gradle.BaseExtension
+                if (baseExtension != null && baseExtension.namespace.isNullOrEmpty()) {
+                    val groupName = project.group.toString()
+                    baseExtension.namespace = if (groupName.isNotEmpty()) groupName else "com.darkmatter.telstream.${project.name.replace("-", "_").replace(":", "_")}"
+                }
+            }
+        }
+    }
+}
+
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
