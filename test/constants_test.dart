81af6c3886fde91fa118ad7df2ff11156c6d518e
WorkerOfArea51 <area.51.ancientworkers@gmail.com>
2026-07-01 02:20:35 +0600

    fix(sync): implement hybrid scorer grouping parser and bump version to 2.9.4

--- test/constants_test.dart
+++ test/constants_test.dart
@@ -5,7 +5,7 @@ void main() {
   group('Constants Tests', () {
     test('Current version is defined and not empty', () {
       expect(Constants.currentVersion, isNotEmpty);
-      expect(Constants.currentVersion, '2.9.3');
+      expect(Constants.currentVersion, '2.9.4');
     });
 
     test('Changelog is defined and contains current version info', () {
