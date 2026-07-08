2beb5ababce42e25d4fab8b129c2a37d3d0c7b4b
WorkerOfArea51 <area.51.ancientworkers@gmail.com>
2026-07-08 22:07:40 +0600

    Fix: Add missing dart:typed_data import for Uint8List in backup_manager_screen

--- lib/features/settings/backup_manager_screen.dart
+++ lib/features/settings/backup_manager_screen.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 import 'dart:io';
+import 'dart:typed_data';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:file_picker/file_picker.dart';
